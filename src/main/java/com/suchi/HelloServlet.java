bbcee88c37a7dbf827b066233d4186d7c9f9a66c
sachin <sachinmensi@gmail.com>
2025-05-12 20:15:16 +0000

    source files

---
+++ src/main/java/com/suchi/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.suchi;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+	    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		            res.setContentType("text/html");
+			            PrintWriter out = res.getWriter();
+				            out.println("<h1>Hello from Suchi's WAR!</h1>");
+					        }
+}
